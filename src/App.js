9c75a0a31478eba2a7efc6fade454e87b29004e4
kwak.kuwon <kwak.kuwon@joins.com>
2020-03-30 22:21:06 +0900

    mockup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import fake1 from './assets/images/fake1.png';
+import fake2 from './assets/images/fake2.png';
+import shark1 from './assets/images/jows1.png';
+import shark2 from './assets/images/jows2.png';
+import shark3 from './assets/images/jows3.png';
 import './App.css';
 
 function App() {
+  const [fake, setFake] = useState(fake1);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{width:"1000px",margin:"0 auto",position:"relative"}}>
+      <img src={shark1} className="shark1"/>
+      <img src={shark2} className="shark2"/>
+      <img src={shark3} className="shark3"/>
+      <h1><span role="img">🦈</span>와 BunDeGi</h1>
+      <img src={fake} alt="죠습니다" style={{cursor:"pointer"}} onClick={_=>(fake === fake1) ? setFake(fake2) : setFake(fake1)}/>
     </div>
   );
 }
