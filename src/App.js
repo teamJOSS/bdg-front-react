9b2d4ed65c4d492dfbf1ad067cf32764020d8b76
kwak.kuwon <kwak.kuwon@joins.com>
2020-04-12 20:23:32 +0900

    시연용 라우터 가이드(임시)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
+import styled from 'styled-components'
+
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Notices from './pages/Notices';
@@ -13,8 +15,23 @@ function App() {
 			<Route path="/login" component={Login} />
       <Route path="/notices" component={Notices} />
       <Route path="/settings" component={Settings} />
+      <DivRouterGuide>
+        <Link to="/login">로그인 페이지</Link>
+        <Link to="/notices">공지사항 페이지</Link>
+        <Link to="/settings">세팅 페이지</Link>
+      </DivRouterGuide>
     </>
   );
 }
+
+const DivRouterGuide = styled.div`
+  padding:14px;
+  position:absolute;
+  top:0;
+  right:0;
+  background:skyblue;
+  display:flex;
+  flex-direction: column;
+`;
 
 export default App;
