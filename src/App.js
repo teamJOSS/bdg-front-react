2f0c0c59c191fc0bc7fb3b5886833d1fa5e606ff
kwak.kuwon <kwak.kuwon@joins.com>
2020-04-12 18:02:43 +0900

    리액트 라우터 설정

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import './App.css';
+import { Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Notices from './pages/Notices';
+import Settings from './pages/Settings';
 
 function App() {
   return (
     <>
+			<Route exact path="/" component={Home} />
+			<Route path="/login" component={Login} />
+      <Route path="/notices" component={Notices} />
+      <Route path="/settings" component={Settings} />
     </>
   );
 }
---
+++ src/pages/Settings.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Settings() {
+  return (
+    <>
+      설정
+    </>
+  )
+}
+
+export default Settings;
