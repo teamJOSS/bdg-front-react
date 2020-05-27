005d5970a61ef7e4f88270999aba10ae2d556e96
kwak.kuwon <kwak.kuwon@joins.com>
2020-05-27 17:52:53 +0900

    공지사항과 Home 레이아웃 작업

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,15 +9,39 @@ function Home() {
   return (
     <SectionHome>
       <NoticeBar/>
-      <PatientTable/>
-      <DateSelector/>
-      <PatientCheckup/>
+      <PatientTableContainer>
+        <PatientTable/>
+      </PatientTableContainer>
+      <DateSelectorContainer>
+        <DateSelector/>
+      </DateSelectorContainer>
+      <PatientCheckupContainer>
+        <PatientCheckup/>
+      </PatientCheckupContainer>
     </SectionHome>
   )
 }
 export default Home;
 
 const SectionHome = styled.section`
+  display:flex;
+  flex-direction:column;
   margin:30px 80px;
   width:100%;
+  max-width: 1320px;
+`;
+const PatientTableContainer = styled.article`
+  background:pink;
+  max-width:880px;
+  min-height:784px;
+`;
+const DateSelectorContainer = styled.div`
+background:skyblue;
+  max-width:420px;
+height:10px;
+`;
+const PatientCheckupContainer = styled.div`
+background:red;
+  max-width:420px;
+height:10px;
 `;
